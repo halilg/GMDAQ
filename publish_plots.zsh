028869dedd40634ecf9bdee8d9c665f86e27ecfd
Halil Gamsizkan <halilgamsizkan@gmail.com>
2015-07-13 15:26:45 +0300

    minor fix

--- publish_plots.zsh
+++ publish_plots.zsh
@@ -2,7 +2,10 @@
 ODIR=/home/halil/www/gmlog
 WDIR=/home/halil/work/GMDAQ
 cd $WDIR
-./make_plots.exe data/gm_00019.txt || exit
+runnum=$(./whichRun.py)
+./make_sql_histos.exe $runnum
+./make_plots_sql.exe $runnum
+#./make_plots.exe data/gm_00019.txt || exit
 mv last_hour.png $ODIR 
 
 
