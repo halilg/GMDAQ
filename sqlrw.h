d4a08d9b919038d66e6ed1c6f2410dc85c178935
Halil Gamsızkan <halilgamsizkan@gmail.com>
2015-07-13 15:17:28 +0300

    created files

---
+++ sqlrw.h
@@ -0,0 +1,35 @@
+#ifndef SQLRW_H
+#define SQLRW_H
+
+#include <string>
+#include <sqlite3.h>
+#include "epoch_histo.h"
+
+class sqlrw {
+    private:
+        sqlite3 *db;
+        bool fileopened;
+        char *zErrMsg;
+        
+    public:
+        int logLevel;
+        epoch_histo minutescnt;
+        epoch_histo hourscnt;
+        epoch_histo dayscnt;
+        sqlrw();
+        ~sqlrw();
+        void create(std::string);
+        void open(std::string);
+        void close();
+        void writeMeta();
+        void readMeta();
+        void setMin(int, int);
+        int getMin(int);
+        void readGMHits(std::string);
+        sqlite3 * getDB(){return db;};
+        void mergeMin();
+        unsigned long lastepmilli;
+        int runNum;
+};
+
+#endif
