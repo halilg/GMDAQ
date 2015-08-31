a946e20628392fe131ea7e5070f68307fefcb605
Halil Gamsizkan <halilgamsizkan@gmail.com>
2015-08-31 17:40:18 +0300

    fixes

--- runs.py
+++ runs.py
@@ -4,7 +4,7 @@ import os,sys,re, sqlite3, time
 
 
 dataDir="data"
-p = re.compile("gm_\d\d\d\d\d\.dat", re.IGNORECASE)
+p = re.compile("gm_\d\d\d\d\d\.dat$", re.IGNORECASE)
 files=os.listdir(dataDir)
 files= filter(p.match, files)
 
@@ -16,8 +16,8 @@ def epoch2DateTime(ep):
 def readRunsData():
     con = None
     for dbfile in files:
-        #print dbfile
-        runsdata[dbfile] = [-1, "?", "?"]
+        # print dbfile
+        runsdata[dbfile] = [-1, "?", "?", 0]
         try:
             con = sqlite3.connect(os.path.join(dataDir,dbfile))
             cur = con.cursor()
@@ -25,7 +25,11 @@ def readRunsData():
             # print(cur.fetchall())
             cur.execute("SELECT * FROM log")
             alldata=cur.fetchall()
-            runsdata[dbfile] = [len(alldata), epoch2DateTime(alldata[0][0]/1000.), epoch2DateTime(alldata[-1][0]/1000.)]
+            if len(alldata) > 0:
+                runsdata[dbfile] = [len(alldata), epoch2DateTime(alldata[0][0]/1000.), epoch2DateTime(alldata[-1][0]/1000.), \
+                                    (alldata[-1][0]-alldata[0][0])/1000./60./60./24.]
+            else:
+                runsdata[dbfile] = [0, 'N/A', 'N/A', 0]
             # print runsdata
              
             
@@ -43,7 +47,8 @@ def readRunsData():
 
 readRunsData()
 
-print "Run#          #Hits                 Start                   End"
-print "---------------------------------------------------------------"  
+print "Run#                 #Hits                 Start                   End     Days"
+print "-------------------------------------------------------------------------------"  
 for k in sorted(runsdata.keys()):
-    print "%2s       %7d   %19s   %19s" % (k[3:8], runsdata[k][0], runsdata[k][1], runsdata[k][2])
+    try: print "%2s       %7d   %19s   %19s     %4.1f" % (k, runsdata[k][0], runsdata[k][1], runsdata[k][2], runsdata[k][3])
+    except: print runsdata[k]
