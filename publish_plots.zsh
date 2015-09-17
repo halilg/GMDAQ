8c675747de6449a9ed546a09df7414d16aabbcab
Halil Gamsizkan <halilgamsizkan@gmail.com>
2015-09-17 14:16:56 +0300

    small fix

--- publish_plots.zsh
+++ publish_plots.zsh
@@ -7,6 +7,8 @@ runnum=$(./runs.py -c)
 ./make_sql_histos.exe $runnum
 ./make_plots_sql.exe $runnum
 #./make_plots.exe data/gm_00019.txt || exit
-mv C_24h.png hPa_24h.png last_hour.png  muT_24h.png index.html $ODIR
-
+mv last_hour.png  index.html $ODIR
+[[ -e  C_24h.png ]] && mv C_24h.png $ODIR
+[[ -e hPa_24h.png ]] && mv hPa_24h.png $ODIR
+[[ -e muT_24h.png ]] && mv muT_24h.png $ODIR
 
