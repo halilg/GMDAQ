d8859160877a545106d0e1cc0b99c2dec840cb69
Halil Gamsizkan <halilgamsizkan@gmail.com>
2015-09-17 14:26:22 +0300

    small fix

--- publish_plots.zsh
+++ publish_plots.zsh
@@ -3,12 +3,17 @@ ODIR=/home/halil/www/gmlog
 WDIR=/home/halil/work/GMDAQ
 cd $WDIR
 runnum=$(./runs.py -c)
+if [[ $# -eq 1 ]]; then
+   if [[ $1 == "-e" ]]; then
+       echo creating environment plots
+       ./make_plots_env.exe $runnum
+       mv C_24h.png hPa_24h.png muT_24h.png $ODIR
+   fi
+fi
+
 ./make_html.py index.html
 ./make_sql_histos.exe $runnum
 ./make_plots_sql.exe $runnum
 #./make_plots.exe data/gm_00019.txt || exit
 mv last_hour.png  index.html $ODIR
-[[ -e  C_24h.png ]] && mv C_24h.png $ODIR
-[[ -e hPa_24h.png ]] && mv hPa_24h.png $ODIR
-[[ -e muT_24h.png ]] && mv muT_24h.png $ODIR
 
