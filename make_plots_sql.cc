bc2439e6da1cc832fffe6690630e4e006801bf1f
Halil Gamsızkan <halilgamsizkan@gmail.com>
2015-09-17 14:34:21 +0300

    small fix

--- make_plots_sql.cc
+++ make_plots_sql.cc
@@ -159,6 +159,7 @@ int main(int argc, char **argv){
     float xoffset=0.53;
     float yoffset=0.15;
     TCanvas c("c", "c", scale*640,scale*325);
+    c.SetGrid();
     //h_hitspm1h.SetMaximum()
     h_hitspm1h.SetMinimum(0);
     h_hitspm1h.Draw("E");
