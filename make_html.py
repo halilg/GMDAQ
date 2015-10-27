bd7116b61413bcf79fbddcb34e2ad93e9438cbe1
Halil Gamsizkan <halilgamsizkan@gmail.com>
2015-10-27 15:18:22 +0300

    text fix

--- make_html.py
+++ make_html.py
@@ -38,12 +38,12 @@ window.onload = function()
 <meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="keywords" content="Geiger-Muller Counter, Geiger Sayacı, Cosmic Rays, Kozmik Işınlar, Muons, Müonlar, Physics, Fizik">
-</head>m
+</head>
 <body>
 <table>
 <tr> <td align='right'> Alış #:</td> <td style="font-family:Courier; color:blue;"> %RUN% </td> </tr>
 <tr> <td align='right'> Süre (gün):</td> <td style="font-family:Courier; color:blue;"> %DAYS% </td> </tr>
-<tr> <td align='right'> Toplam GM vuruş sayısı:</td> <td style="font-family:Courier; color:blue;"> %HITS% </td> </tr>
+<tr> <td align='right'> Toplam müon sayısı:</td> <td style="font-family:Courier; color:blue;"> %HITS% </td> </tr>
 </table>
  
 <!-- Started: %STARTED% <br/> -->
